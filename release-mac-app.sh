be34f351706a49351c8fd4c384b25986a31bc619
Patrick Coffey <patrick.coffey@lufthansa-group.com>
2026-09-01 17:04:35 +0200

    Add sha265 generation I need this hash for the homebrew process

--- release-mac-app.sh
+++ release-mac-app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Making release for $1. '$2'"
 zip -r JellyJams-$1.zip ~/Applications/JellyJams.app
+echo "SHA265 hash: $(sha256sum JellyJams-$1.zip)"
 gh release create v$1 --title "Jelly Jams v$1" --notes "$2" JellyJams-$1.zip
 echo "Done"
