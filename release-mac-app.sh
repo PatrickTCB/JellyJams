80610aad21f6484d4649a83f39d69f0619c4ebed
Patrick Coffey <patrick.coffey@lufthansa-group.com>
2026-09-02 08:51:53 +0200

    App path within zip Fixed the app path inside the zip by copying it from ~/Applications before zipping

--- release-mac-app.sh
+++ release-mac-app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Making release for $1. '$2'"
-zip -r JellyJams-$1.zip ~/Applications/JellyJams.app
+cp ~/Applications/JellyJams.app .
+zip -r JellyJams-$1.zip JellyJams.app
 echo "SHA265 hash: $(sha256sum JellyJams-$1.zip)"
 gh release create v$1 --title "Jelly Jams v$1" --notes "$2" JellyJams-$1.zip
 echo "Done"
