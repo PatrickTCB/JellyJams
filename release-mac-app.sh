3f6da42b8cac9e65c7380d1d08a3b0abdb1ddc1a
Patrick Coffey <patrick.coffey@lufthansa-group.com>
2026-09-02 08:55:14 +0200

    add -r to copy flag

--- release-mac-app.sh
+++ release-mac-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Making release for $1. '$2'"
-cp ~/Applications/JellyJams.app .
+cp -r ~/Applications/JellyJams.app .
 zip -r JellyJams-$1.zip JellyJams.app
 echo "SHA265 hash: $(sha256sum JellyJams-$1.zip)"
 gh release create v$1 --title "Jelly Jams v$1" --notes "$2" JellyJams-$1.zip
