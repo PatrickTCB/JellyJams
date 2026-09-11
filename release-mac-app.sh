bcfe0562f0c7c4e88f443b3aec640aac13077c40
Patrick Coffey <patrick.coffey@lufthansa-group.com>
2026-09-11 13:40:56 +0200

    fixed build script

--- release-mac-app.sh
+++ release-mac-app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "Making release for $1. '$2'"
-sign_update Jelly\ Jams.app
-read -r -p "Update your 'appcast.xml' file with the required values and then press Enter to continue..." _
-gh release create v$1 --title "Jelly Jams v$1" --notes "$2" --asset Jelly\ Jams.app --asset appcast.xml
-echo "Done"
+set -e
+VER=$1
+NOTES=$2
+
+ditto -c -k --sequesterRsrc --keepParent "Jelly Jams.app" "JellyJams-$VER.zip"
+generate_appcast --download-url-prefix "https://github.com/PatrickTCB/JellyJams/releases/download/v$VER/" .
+gh release create "v$VER" --title "Jelly Jams v$VER" --notes "$NOTES" "JellyJams-$VER.zip" appcast.xml
