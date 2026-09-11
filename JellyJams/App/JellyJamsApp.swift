52e6187bfb194b30e29ee959c50473abe101961c
Patrick <github@coffey.app>
2026-09-11 12:07:22 +0200

    add sparkle SDK for updates

--- JellyJams/App/JellyJamsApp.swift
+++ JellyJams/App/JellyJamsApp.swift
@@ -1,4 +1,38 @@
 import SwiftUI
+#if os(macOS)
+import Sparkle
+
+// This view model class publishes when new updates can be checked by the user
+@MainActor
+final class CheckForUpdatesViewModel: ObservableObject {
+    @Published var canCheckForUpdates = false
+
+    init(updater: SPUUpdater) {
+        updater.publisher(for: \.canCheckForUpdates)
+            .assign(to: &$canCheckForUpdates)
+    }
+}
+
+// This is the view for the Check for Updates menu item
+// Note this intermediate view is necessary for the disabled state on the menu item to work properly before Monterey.
+// See https://stackoverflow.com/questions/68553092/menu-not-updating-swiftui-bug for more info
+struct CheckForUpdatesView: View {
+    @ObservedObject private var checkForUpdatesViewModel: CheckForUpdatesViewModel
+    private let updater: SPUUpdater
+    
+    init(updater: SPUUpdater) {
+        self.updater = updater
+        
+        // Create our view model for our CheckForUpdatesView
+        self.checkForUpdatesViewModel = CheckForUpdatesViewModel(updater: updater)
+    }
+    
+    var body: some View {
+        Button("Check for Updates…", action: updater.checkForUpdates)
+            .disabled(!checkForUpdatesViewModel.canCheckForUpdates)
+    }
+}
+#endif
 
 @main
 struct JellyJamsApp: App {
@@ -11,7 +45,15 @@ struct JellyJamsApp: App {
     @StateObject private var settingsPresentation = SettingsPresentation()
     @StateObject private var preferences = PreferencesStore()
     @StateObject private var downloads = DownloadStore()
-
+    #if os(macOS)
+    private let updaterController: SPUStandardUpdaterController
+        
+    init() {
+        // If you want to start the updater manually, pass false to startingUpdater and call .startUpdater() later
+        // This is where you can also pass an updater delegate if you need one
+        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+    }
+    #endif
     var body: some Scene {
         WindowGroup {
             RootContainerView()
@@ -29,6 +71,11 @@ struct JellyJamsApp: App {
         }
         .commands {
             PlaybackCommands(player: player)
+            #if os(macOS)
+            CommandGroup(after: .appInfo) {
+                CheckForUpdatesView(updater: updaterController.updater)
+            }
+            #endif
             CommandGroup(replacing: .newItem) { }
             #if os(iOS)
             SettingsCommands(presentation: settingsPresentation)
