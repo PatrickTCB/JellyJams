cec2483df185bb4ab6a4798e92083c548ff9c479
Patrick Coffey <patrick.coffey@lufthansa-group.com>
2026-09-11 17:27:15 +0200

    delete zip file once I'm done with it

--- release-mac-app.sh
+++ release-mac-app.sh
@@ -6,3 +6,4 @@ NOTES=$2
 ditto -c -k --sequesterRsrc --keepParent "Jelly Jams.app" "JellyJams-$VER.zip"
 generate_appcast --download-url-prefix "https://github.com/PatrickTCB/JellyJams/releases/download/v$VER/" .
 gh release create "v$VER" --title "Jelly Jams v$VER" --notes "$NOTES" "JellyJams-$VER.zip" appcast.xml
+rm JellyJams-$VER.zip
