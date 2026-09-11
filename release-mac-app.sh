e390cbf8d1b1fc69f99d8b7eef7dea09c95dea7b
Patrick <github@coffey.app>
2026-09-11 12:07:53 +0200

    Update release script based on new sparkle stuff

--- release-mac-app.sh
+++ release-mac-app.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Making release for $1. '$2'"
-cp -r ~/Applications/JellyJams.app .
-zip -r JellyJams-$1.zip JellyJams.app
-echo "SHA265 hash: $(sha256sum JellyJams-$1.zip)"
-gh release create v$1 --title "Jelly Jams v$1" --notes "$2" JellyJams-$1.zip
+sign_update Jelly\ Jams.app
+read -r -p "Update your 'appcast.xml' file with the required values and then press Enter to continue..." _
+gh release create v$1 --title "Jelly Jams v$1" --notes "$2" --asset Jelly\ Jams.app --asset appcast.xml
 echo "Done"
